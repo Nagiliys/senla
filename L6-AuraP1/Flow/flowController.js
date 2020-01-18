fe2bb5083fbdd8c7c1468918742db1b8aa137952
Korshun Vadim <nagiliys@mail.ru>
2020-01-18 20:45:59 +0300

    fix 2

--- L6-AuraP1/Flow/flowController.js
+++ L6-AuraP1/Flow/flowController.js
@@ -1,13 +1,10 @@
 ({
     init : function (component, event, helper) {
-    const recordId = component.get("v.recordId");    
-    const pagetype = component.get("v.sObjectName");   
-    if (pagetype == "Account" && recordId!="") {
+        const recordId = component.get("v.recordId");    
+        const pagetype = component.get("v.sObjectName");   
         const flow = component.find("flowData");
         const input = [{ name : "recordId", type : "String", value: recordId}];
         flow.startFlow("update", input);
-    } else {
-        component.set("v.bagdeValue", $A.get("$Label.c.cmpNameErr"));
-    }      
+        component.set("v.cmpNameErr", $A.get("$Label.c.cmpNameErr"))   
   }
 })
--- L6-AuraP1/Randomizer/RandomController.js
+++ L6-AuraP1/Randomizer/RandomController.js
@@ -1,12 +1,21 @@
 ({
 	random : function(component, event, helper) {      
-        const valueFrom = parseInt(component.get("v.valueFrom")); //typeof valueTo and valueFrom equal string
-        const valueTo = parseInt(component.get("v.valueTo"));	 // i don't know why
+        const valueFrom = parseInt(component.get("v.valueFrom"));
+        const valueTo = parseInt(component.get("v.valueTo"));
         if (valueFrom < valueTo){
             const result = parseInt(Math.random() * (valueTo - valueFrom) + valueFrom);
-        component.set("v.result", result);
+        	component.set("v.result", result);
         } else {
             helper.showToast();  
         }
-}
+    },
+    checkFields: function(component, event, helper) {
+  		const valueFrom = component.get("v.valueFrom");
+        const valueTo = component.get("v.valueTo");
+        if (valueFrom == null || valueTo == null || valueFrom == "" || valueTo == "") {
+        	component.set("v.isActiveBtn", true);
+        } else {
+        	component.set("v.isActiveBtn", false);
+    	}
+  }
 })
