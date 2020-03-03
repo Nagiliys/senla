58fd3f2b57d4af315ef2a3617ff917d97a6bbc07
Korshun Vadim <36503888+Nagiliys@users.noreply.github.com>
2020-03-03 13:39:49 +0300

    Update RandomHelper.js

--- L6-AuraP1/Randomizer/RandomHelper.js
+++ L6-AuraP1/Randomizer/RandomHelper.js
@@ -2,15 +2,15 @@
 	showToast: function() {
 		let toastEvent = $A.get("e.force:showToast");
 		const title = $A.get("$Label.c.title_valid_err");
-        const message = $A.get("$Label.c.msg_valid_err");
-        toastEvent.setParams({
-                title : title,
-                message: message,
-                duration:' 5000',
-                key: 'info_alt',
-                type: 'error',
-                mode: 'pester'
-            });
-        toastEvent.fire();
+		const message = $A.get("$Label.c.msg_valid_err");
+		toastEvent.setParams({
+			title : title,
+			message: message,
+			duration:' 5000',
+			key: 'info_alt',
+			type: 'error',
+			mode: 'pester'
+		    });
+		toastEvent.fire();
 	}    
 })
